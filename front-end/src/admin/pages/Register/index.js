7059dd3c408591b29e2aed29f15d0ebf6151c7b3
henrydemon <79507043+henrydemon@users.noreply.github.com>
2023-04-06 22:23:56 +0700

    first push

---
+++ front-end/src/admin/pages/Register/index.js
@@ -0,0 +1,11 @@
+import { connect } from "react-redux";
+
+import Register from "./Register";
+
+const mapStateToProps = (state) => ({
+});
+
+const mapDispatchToProps = {
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
